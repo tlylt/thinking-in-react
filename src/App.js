ba62e4df2b294786226ecb3c1d16a855828a3700
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2020-08-22 00:34:33 +0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+function StepNumberBar({ total }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="stepNumberBar">
+      {Array(total)
+        .fill(null)
+        .map((value, index) => (
+          <span id={index} key={index}>
+            {index}
+          </span>
+        ))}
     </div>
   );
+}
+
+function KeyList({ content }) {
+  var itemsArr = content.split(".");
+  return (
+    <ul>
+      {itemsArr.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+function Explanation({ heading, content }) {
+  return (
+    <div className="explanation">
+      <h2>{heading}</h2>
+      <KeyList content={content} />
+    </div>
+  );
+}
+function NavigationRow({ updatePage }) {
+  return (
+    <div className="buttons">
+      <MoveStepButton updatePage={updatePage} direction="prev" />
+      <MoveStepButton updatePage={updatePage} direction="next" />
+    </div>
+  );
+}
+
+function MoveStepButton({ updatePage, direction }) {
+  return direction === "prev" ? (
+    <button onClick={() => updatePage((curr) => (curr === 0 ? 5 : curr - 1))}>
+      PREV
+    </button>
+  ) : (
+    <button onClick={() => updatePage((curr) => (curr === 5 ? 0 : curr + 1))}>
+      NEXT
+    </button>
+  );
+}
+
+function ReferenceTable({ detail }) {
+  const [currPage, updatePage] = useState(0);
+  useEffect(() => {
+    var currP = document.querySelector("span[id='" + currPage + "']");
+    var allSpan = document.querySelectorAll("span");
+    allSpan.forEach((item) => {
+      item.style.color = "black";
+      item.className = item.className.replace(/active/g, "");
+    });
+    currP.style.color = "grey";
+    currP.classList += ["active"];
+  }, [currPage]);
+  return (
+    <>
+      <StepNumberBar total={detail.length} current={currPage} />
+      <Explanation
+        heading={detail[currPage].heading}
+        content={detail[currPage].content}
+      />
+      <NavigationRow updatePage={updatePage} />
+    </>
+  );
+}
+
+function App(props) {
+  return <ReferenceTable detail={props.detail} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const data = [
+  {
+    heading: "Start With A Mock",
+    content: "Front end in its plain form",
+  },
+  {
+    heading: "Break The UI Into A Component Hierarchy",
+    content:
+      "Draw boxes.Identify components (Single Responsibility Principle).Arrange into hierarchy",
+  },
+  {
+    heading: "Build A Static Version",
+    content:
+      "Don't use state at all, only use props.Reuse components.Top down/Bottom up to you.Pass sample\
+      data to the top of the hierarchy",
+  },
+  {
+    heading: "Identify The Minimal Representation of UI State",
+    content:
+      "Keep only the absolute minimal and compute everything else on-demand.Is it passed in from a parent via props?If so, it probably isn't state.\
+      Does it remain unchanged over time? If so, it probably isn’t state.Can you compute it based on any other state or props in your component? If so, it isn’t state",
+  },
+  {
+    heading: "Identify Where Your State Should Live",
+    content:
+      "Identify every component that renders something based on that state.Find a common owner component (above all other components).\
+      Create a wrapper component above components if necessary",
+  },
+  {
+    heading: "Add Inverse Data Flow",
+    content:
+      "Pass state changing callbacks from state owner to relevant child component",
+  },
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App detail={data} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
